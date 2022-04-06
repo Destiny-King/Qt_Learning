3b957a05dc28a650601c6cb1fc3d5dabecd56b76
Destiny-King <2681283464@qq.com>
2022-04-06 19:26:52 +0800

    add sortdialog

---
+++ dialog/sortdialog.h
@@ -0,0 +1,21 @@
+#ifndef SORTDIALOG_H
+#define SORTDIALOG_H
+
+#include <QDialog>
+#include "ui_sortdialog.h"
+
+class SortDialog : public QDialog,public Ui::SortDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SortDialog(QWidget *parent = 0);
+
+    void setColumnRange(QChar first,QChar last);
+    ~SortDialog();
+
+private:
+
+};
+
+#endif // SORTDIALOG_H
