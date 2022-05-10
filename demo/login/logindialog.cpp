b19a872bf2b5101573ce8b5cf521806803fd51d9
Destiny-King <2681283464@qq.com>
2022-05-10 13:55:12 +0800

    add login

---
+++ demo/login/logindialog.cpp
@@ -0,0 +1,54 @@
+#include "logindialog.h"
+#include "ui_logindialog.h"
+#include "QLabel"
+#include "QLineEdit"
+#include "QPushButton"
+#include "QMessageBox"
+
+LoginDialog::LoginDialog(QWidget *parent) :
+    QDialog(parent)
+{
+//    ui->setupUi(this);
+    usrLabel = new QLabel(this);
+    usrLabel->move(70,80);
+    usrLabel->setText(tr("用户名"));
+    usrLineEdit = new QLineEdit(this);
+    usrLineEdit->move(140,80);
+    usrLineEdit->setPlaceholderText(tr("请输入用户名"));
+
+    pwdLabel = new QLabel(this);
+    pwdLabel->move(70,130);
+    pwdLabel->setText(tr("密码"));
+    pwdLineEdit = new QLineEdit(this);
+    pwdLineEdit->move(140,130);
+    pwdLineEdit->setPlaceholderText("请输入密码");
+
+    loginBtn = new QPushButton(this);
+    loginBtn->move(50,200);
+    loginBtn->setText(tr("登录"));
+    exitBtn = new QPushButton(this);
+    exitBtn->move(210,200);
+    exitBtn->setText(tr("退出"));
+
+    pwdLineEdit->setEchoMode(QLineEdit::Password);
+
+    connect(loginBtn,&QPushButton::clicked,this,&LoginDialog::login);
+    connect(exitBtn,&QPushButton::clicked,this,&LoginDialog::close);
+}
+
+LoginDialog::~LoginDialog()
+{
+//    delete ui;
+}
+
+void LoginDialog::login()
+{
+    if(usrLineEdit->text().trimmed()==tr("admin")&&pwdLineEdit->text()==tr("123456")){
+        accept();
+    }else{
+        QMessageBox::warning(this,tr("警告"),tr("用户名或密码错误！"),tr("确定"));
+        usrLineEdit->clear();
+        pwdLineEdit->clear();
+        usrLineEdit->setFocus();
+    }
+}
